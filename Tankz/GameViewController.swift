f1edfa81c171e30fb79cc79be8e7a28f197fb185
langokalla <martin.langmo.karlstrom@gmail.com>
2018-03-06 16:13:58 +0100

    Added Tank, TankFactory, Map and MapFactory classes. Can generate map and tanks. Physics up and running.

--- Tankz/GameViewController.swift
+++ Tankz/GameViewController.swift
@@ -14,6 +14,24 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .resizeFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            //Debug stats
+            view.showsFPS = false
+            view.showsNodeCount = false
+            view.showsPhysics = true
+        }
     }
 
     override var shouldAutorotate: Bool {
