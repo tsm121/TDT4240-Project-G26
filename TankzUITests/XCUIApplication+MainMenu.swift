2908f1bdc4457f919253494f5a496bb1b4d6bbc2
langokalla <martin.langmo.karlstrom@gmail.com>
2018-03-07 15:34:31 +0100

    UITesting up and running

--- TankzUITests/XCUIApplication+MainMenu.swift
+++ TankzUITests/XCUIApplication+MainMenu.swift
@@ -2,7 +2,7 @@
 //  XCUIApplication+MainMenu.swift
 //  TankzUITests
 //
-//  Created by Martin Langmo Karlstrøm on 06.03.2018.
+//  Created by Martin Langmo Karlstrøm on 07.03.2018.
 //  Copyright © 2018 TDT4240-Group26. All rights reserved.
 //
 
