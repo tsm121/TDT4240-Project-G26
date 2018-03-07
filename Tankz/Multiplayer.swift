657db7d5ad3c3151cfb7a11ffd576ec16d1c263f
Clas Olaf Steinbru Andersen <coanders@stud.ntnu.no>
2018-03-07 10:45:16 +0100

    extended skeleton added foundPeer functionality added lostPeer functionality added getGames that returns an array of MCPeerID of available games

--- Tankz/Multiplayer.swift
+++ Tankz/Multiplayer.swift
@@ -5,16 +5,18 @@ import MultipeerConnectivity
  * Handles all the functionality related to multiplayer.
  * Debug using $(dns-sd -B _services._dns-sd._udp).
  */
-class Multiplayer {
+class Multiplayer : NSObject {
     
     private let type = "tankz"
     private let peerID = MCPeerID(displayName: UIDevice.current.name)
     private let browser : MCNearbyServiceBrowser
     private let advertiser : MCNearbyServiceAdvertiser
+    var games = [MCPeerID]()
     
-    init () {
+    override init () {
         self.browser = MCNearbyServiceBrowser(peer: self.peerID, serviceType: self.type)
         self.advertiser = MCNearbyServiceAdvertiser(peer: self.peerID, discoveryInfo: nil, serviceType: self.type)
+        super.init()
     }
     
     lazy var session : MCSession = {
@@ -25,7 +27,7 @@ class Multiplayer {
     
     /* Advertise this client as a host for other clients. */
     func advertiseAsHost() {
-        self.advertiser.delegate = self as? MCNearbyServiceAdvertiserDelegate
+        self.advertiser.delegate = self
         self.advertiser.startAdvertisingPeer()
     }
     
@@ -34,17 +36,53 @@ class Multiplayer {
         self.advertiser.stopAdvertisingPeer()
     }
     
-    /* Look for other hosts. */
-    func lookForHosts() {
+    /* Look for games. */
+    func lookForGames() {
         self.browser.startBrowsingForPeers()
     }
     
-    /* Cease looking for peers. */
-    func ceaseLookingForPeers() {
+    /* Cease looking for games. */
+    func ceaseLookingForGames() {
         self.browser.stopBrowsingForPeers()
+    }
+    
+    /* Get found games. */
+    func getGames() -> [MCPeerID] {
+        return games
     }
     
     /* Join existing game. */
     
     /* Mark as ready to play. */
+}
+
+extension Multiplayer : MCNearbyServiceBrowserDelegate {
+    
+    func browser(_ browser: MCNearbyServiceBrowser, didNotStartBrowsingForPeers error: Error) {
+        NSLog("%@", "didNotStartBrowsingForPeers: \(error)")
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        NSLog("%@", "foundPeer: \(peerID)")
+        self.games.append(peerID)
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        NSLog("%@", "lostPeer: \(peerID)")
+        let index = self.games.index(of: peerID)
+        self.games.remove(at: index!)
+    }
+    
+}
+
+extension Multiplayer : MCNearbyServiceAdvertiserDelegate {
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: Error) {
+        NSLog("%@", "didNotStartAdvertisingPeer: \(error)")
+    }
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        NSLog("%@", "didReceiveInvitationFromPeer \(peerID)")
+    }
+    
 }
