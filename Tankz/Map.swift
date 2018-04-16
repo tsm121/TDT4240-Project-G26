5b4009a03ac848b20ef64f6e33aa5357cfeb8cbb
langokalla <martin.langmo.karlstrom@gmail.com>
2018-04-17 00:01:19 +0200

    Different maps with different colors.

--- Tankz/Map.swift
+++ Tankz/Map.swift
@@ -12,18 +12,29 @@ class Map {
     
     let height : CGFloat
     var ground : SKShapeNode
+    let backgroundHeight : CGFloat
+    var wallpaper : SKShapeNode
     
     init(mapType: MapType, width: CGFloat) {
+        self.height = 20
+        
         switch mapType {
-        case .flat:
-            self.height = 0
-        case .flatty:
-            self.height = 20
-        case .hills:
-            self.height = 15
+        case .earth:
+            self.backgroundHeight = 200
+        case .mars:
+            self.backgroundHeight = 250
+        case .moon:
+            self.backgroundHeight = 300
         }
         
         self.ground = SKShapeNode()
+        self.wallpaper = SKShapeNode()
+        
+        makeGround(mapType: mapType, width: width)
+        makeWallpaper(mapType: mapType, width: width)
+    }
+    
+    func makeGround(mapType: MapType, width: CGFloat) {
         
         let interval = [0, width*(1/5), width*(2/5), width*(3/5), width*(4/5), width]
         var points = [CGPoint(x: Int(interval[0]), y: randHeight(height: height)),
@@ -46,17 +57,22 @@ class Map {
         
         // Convert the path to a SKShapeNode and set colors and stuff.
         self.ground = SKShapeNode(path: beizer.cgPath)
-        self.ground.strokeColor = UIColor(named: "lightGreen")!
         self.ground.lineWidth = 1
-        self.ground.fillColor = UIColor(named: "groundBrown")!
         
+        // Map colors for the ground.
         switch mapType {
-        case .flat:
-            self.ground.name = "FlatGround"
-        case .flatty:
-            self.ground.name = "FlattyGround"
-        case .hills:
-            self.ground.name = "HillsGround"
+        case .earth:
+            self.ground.name = "EarthGround"
+            self.ground.fillColor = UIColor(named: "groundBrown")!
+            self.ground.strokeColor = UIColor(named: "lightGreen")!
+        case .mars:
+            self.ground.name = "MarsGround"
+            self.ground.fillColor = UIColor(red: 247/255, green: 159/255, blue: 121/255, alpha: 1)
+            self.ground.strokeColor = UIColor(red: 247/255, green: 159/255, blue: 121/255, alpha: 1)
+        case .moon:
+            self.ground.name = "MoonGround"
+            self.ground.fillColor = UIColor(red: 194/255, green: 187/255, blue: 240/255, alpha: 1)
+            self.ground.strokeColor = UIColor(red: 194/255, green: 187/255, blue: 240/255, alpha: 1)
         }
         
         // Give it some physics.
@@ -68,7 +84,54 @@ class Map {
         self.ground.physicsBody!.categoryBitMask = PhysicsCategory.Ground
         self.ground.physicsBody!.collisionBitMask = PhysicsCategory.Projectile | PhysicsCategory.Tank
         self.ground.physicsBody!.contactTestBitMask = PhysicsCategory.Projectile
+        
     }
+    
+    func makeWallpaper(mapType: MapType, width: CGFloat) {
+        
+        let interval = [0, width*(1/5), width*(2/5), width*(3/5), width*(4/5), width]
+        var points = [CGPoint(x: Int(interval[0]), y: randHeight(height: backgroundHeight)),
+                      CGPoint(x: Int(interval[1]), y: randHeight(height: backgroundHeight)),
+                      CGPoint(x: Int(interval[2]), y: randHeight(height: backgroundHeight)),
+                      CGPoint(x: Int(interval[3]), y: randHeight(height: backgroundHeight)),
+                      CGPoint(x: Int(interval[4]), y: randHeight(height: backgroundHeight)),
+                      CGPoint(x: Int(interval[5]), y: randHeight(height: backgroundHeight))]
+        
+        
+        // Draw a BeizerPath from the points. Making it SMOOTH AF.
+        let spline = SKShapeNode(splinePoints: &points, count: points.count)
+        
+        // Add straight lines at border to make closed beizer path.
+        let beizer = UIBezierPath(cgPath: spline.path!)
+        beizer.move(to: points.first!)
+        beizer.addLine(to: CGPoint(x: 0, y: 0))
+        beizer.addLine(to: CGPoint(x: width, y: 0))
+        beizer.addLine(to: points.last!)
+        
+        // Convert the path to a SKShapeNode and set colors and stuff.
+        self.wallpaper = SKShapeNode(path: beizer.cgPath)
+        self.wallpaper.lineWidth = 1
+        
+        // Map colors for the ground.
+        switch mapType {
+        case .earth:
+            self.wallpaper.name = "EarthGround"
+            self.wallpaper.fillColor = UIColor(named: "lightGreen")!.withAlphaComponent(0.5)
+            self.wallpaper.strokeColor = UIColor(named: "lightGreen")!.withAlphaComponent(0.5)
+        case .mars:
+            self.wallpaper.name = "MarsGround"
+            self.wallpaper.fillColor = UIColor(red: 247/255, green: 208/255, blue: 138/255, alpha: 0.5)
+            self.wallpaper.strokeColor = UIColor(red: 247/255, green: 208/255, blue: 138/255, alpha: 0.5)
+        case .moon:
+            self.wallpaper.name = "MoonGround"
+            self.wallpaper.fillColor = UIColor(red: 241/255, green: 227/255, blue: 243/255, alpha: 0.5)
+            self.wallpaper.strokeColor = UIColor(red: 241/255, green: 227/255, blue: 243/255, alpha: 0.5)
+        }
+    }
+    
+    
+    
+        
     
     func randHeight(height: CGFloat) -> Int{
         return Int(arc4random_uniform(UInt32(height))) + Int(50)
